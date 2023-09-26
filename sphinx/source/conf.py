a7f2518bc72a7639c7276f1905d7ce62c37964bd
nanli <linanenv@gmail.com>
2023-09-26 12:07:42 +0800

    fix docs width

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -35,5 +35,9 @@ exclude_patterns = ["AI.rst"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_css_files = [
+    "css/custom.css",
+]
+
